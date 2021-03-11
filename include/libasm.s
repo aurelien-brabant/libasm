a7ca2e0e930a09a54afc85d7be1af40524c6773f
abrabant <abrabant@student.42.fr>
2021-03-11 22:38:42 +0100

    Add include file

---
+++ include/libasm.s
@@ -0,0 +1,21 @@
+; **************************************************************************** #
+;                                                                              #
+;                                                         :::      ::::::::    #
+;    libasm.s                                           :+:      :+:    :+:    #
+;                                                     +:+ +:+         +:+      #
+;    By: abrabant <abrabant@student.42.fr>          +#+  +:+       +#+         #
+;                                                 +#+#+#+#+#+   +#+            #
+;    Created: 2021/03/10 09:53:52 by abrabant          #+#    #+#              #
+;    Updated: 2021/03/10 09:53:52 by abrabant         ###   ########.fr        #
+;                                                                              #
+; **************************************************************************** #
+
+%ifndef LIBASM_H
+	%define LIBASM_H
+	extern ft_strlen
+	extern ft_strdup
+	extern ft_strcmp
+	extern ft_strcpy
+	extern ft_write
+	extern ft_read
+%endif
